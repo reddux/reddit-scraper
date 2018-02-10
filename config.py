699a5af3ab29f56a8cabc5ddb9725a0be352f094
Andy Mai <hi@andymai.com>
2018-02-10 13:37:16 -0800

    update env var

--- config.py
+++ config.py
@@ -1,13 +1,10 @@
 import os
 
 # Settings 
-
 USER_AGENT = "Posts Saver Bot v0.1"
 MIN_UPVOTES = 1000
 MIN_COMMENTS = 2
-START_RANGE = 1483228800
-END_RANGE = 1514764799
-SUBREDDIT_NAME = os.environ.get('SUBREDDIT_NAME', 'writingprompts')
+SUBREDDIT_NAME = 'writingprompts'
 
 # PRAW Constants
 REDDIT_USERNAME = os.environ.get('REDDIT_USERNAME')
